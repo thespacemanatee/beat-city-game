d0b503c1679b91fae0deaf5431418f6f786f2aa8
yikkhuen-entiis <yikkhuen.kong@entiis.com>
2022-07-03 13:40:09 +0800

    Extend of the character and health script

---
+++ Assets/Scenes/Prefabs/KillDeath/NewCharacter.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
+using MoreMountains.TopDownEngine;
+using System.Collections.Generic;
+using System;
+using Random = UnityEngine.Random;
+using UnityEngine.Events;
+
+public class NewCharacter : Character
+{
+    public NewHealth NewCharacterHealth;
+    public CustomKDEvent onPlayerDeath;
+    protected virtual void NewOnDeath(GameObject instigator)
+    {
+        if (CharacterBrain != null)
+        {
+            CharacterBrain.TransitionToState("");
+            CharacterBrain.enabled = false;
+        }
+        if (MovementState.CurrentState != CharacterStates.MovementStates.FallingDownHole)
+        {
+            MovementState.ChangeState(CharacterStates.MovementStates.Idle);
+        }
+        Debug.Log("New On Death Triggered");
+        Debug.Log("Killer:");   
+        Debug.Log(instigator.GetComponent<Projectile>().Owner.GetComponent<Character>().PlayerID);    
+        string killer = instigator.GetComponent<Projectile>().Owner.GetComponent<Character>().PlayerID;
+        Debug.Log("Death:");
+        Debug.Log(GetComponent<Character>().PlayerID);
+        string victim = GetComponent<Character>().PlayerID;
+        onPlayerDeath.Invoke(killer, victim);
+    }
+    /// <summary>
+    /// OnEnable, we register our OnRevive event
+    /// </summary>
+    protected override void OnEnable ()
+    {
+        if (CharacterHealth != null)
+        {
+            if (!_onReviveRegistered)
+            {
+                CharacterHealth.OnRevive += OnRevive;
+                _onReviveRegistered = true;
+            }
+            CharacterHealth.OnDeath += OnDeath;
+            NewCharacterHealth.NewOnDeath += NewOnDeath;
+            CharacterHealth.OnHit += OnHit;
+        }
+    }
+
+    /// <summary>
+    /// OnDisable, we unregister our OnRevive event
+    /// </summary>
+    protected override void OnDisable()
+    {
+        if (CharacterHealth != null)
+        {
+            CharacterHealth.OnDeath -= OnDeath;
+            NewCharacterHealth.NewOnDeath -= NewOnDeath;
+            CharacterHealth.OnHit -= OnHit;
+        }			
+    }
+}
