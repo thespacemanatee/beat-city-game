0f89146474c4cd9211d7d51d027cf8910c98a6e1
Chee Kit <cheekit.chong98@gmail.com>
2022-07-03 21:31:43 +0800

    feat(energy): add pooler and spawner

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using MoreMountains.Tools;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public MMSimpleObjectPooler pooler;
+
+    private void Start()
+    {
+        // spawn two energy packs
+        for (var j = 0; j < 20; j++)
+            SpawnFromPooler();
+        StartCoroutine(SpawnLooper());
+    }
+
+    void SpawnFromPooler()
+    {
+        // static method access
+        var item = pooler.GetPooledGameObject();
+        if (item != null)
+        {
+            item.transform.position = new Vector3(Random.Range(-10f, 10f), 10, Random.Range(-10f, 10f));
+            item.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("not enough items in the pool.");
+        }
+    }
+
+    private IEnumerator SpawnLooper()
+    {
+        while (true)
+        {
+            SpawnFromPooler();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
