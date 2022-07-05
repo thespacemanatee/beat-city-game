8b60ff3f43588decc84b7e4d3bf8dffe7939873d
Jukocross <jukocross@gmail.com>
2022-07-05 11:46:36 +0800

    Remove Update check on position

--- Assets/Scripts/KillDeath/NewCharacter.cs
+++ Assets/Scripts/KillDeath/NewCharacter.cs
@@ -6,17 +6,17 @@ public class NewCharacter : Character
 {
     public NewHealth NewCharacterHealth;
     public CustomKDEvent onPlayerDeath;
-    private bool dropped;
+    //private bool dropped;
 
     protected override void Update()
     {
         base.Update();
-        if (GetComponent<Transform>().position.y < 0.0f && !dropped)
-        {
-            var victim = GetComponent<Character>().PlayerID;
-            onPlayerDeath.Invoke(victim, victim);
-            dropped = true;
-        }
+        // if (GetComponent<Transform>().position.y < 0.0f && !dropped)
+        // {
+        //     var victim = GetComponent<Character>().PlayerID;
+        //     onPlayerDeath.Invoke(victim, victim);
+        //     dropped = true;
+        // }
     }
 
     /// <summary>
