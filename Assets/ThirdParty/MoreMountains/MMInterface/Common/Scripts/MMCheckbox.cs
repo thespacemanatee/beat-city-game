a4de3e750dd0f09a36d37b0c453aaa84576dbe0e
Chee Kit <cheekit.chong98@gmail.com>
2022-07-16 14:32:43 +0800

    chore: initial migration

--- Assets/ThirdParty/MoreMountains/MMInterface/Common/Scripts/MMCheckbox.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MoreMountains.MMInterface
-{
-	/// <summary>
-	///     A class to handle checkboxes
-	/// </summary>
-	public class MMCheckbox : MMSpriteReplace
-    {
-    }
-}
--- Assets/ThirdParty/MoreMountains/MMTools/Tools/MMParticles/MMTrailRendererSortingLayer.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace MoreMountains.Tools
-{
-    [AddComponentMenu("More Mountains/Tools/Particles/MMTrailRendererSortingLayer")]
-    public class MMTrailRendererSortingLayer : MonoBehaviour
-    {
-    }
-}
