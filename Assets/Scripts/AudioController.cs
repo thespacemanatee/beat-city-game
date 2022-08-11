4dee063283cb4ea62dd4ee8733f7f7370cb8e247
Claudia Chin <50474688+claudiachin@users.noreply.github.com>
2022-08-11 12:38:18 +0800

    force field changes

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -31,8 +31,6 @@ public class AudioController : MonoBehaviour
             audioData.pitch += Time.deltaTime * 1 / 250;
         }
 
-        Debug.Log("audio pitch " + audioData.pitch);
-
     }
 
     public void stopMusic()
--- Assets/Scripts/Weapon/ForceField.cs
+++ Assets/Scripts/Weapon/ForceField.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ForceField : MonoBehaviour
 {
+    public GameObject forcefield;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Forcefield is " + forcefield);
+        StartCoroutine(SelfDestruct());
     }
 
     // Update is called once per frame
@@ -16,11 +19,18 @@ public class ForceField : MonoBehaviour
         
     }
 
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(5f);
+        forcefield.SetActive(false);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Attack")
         {
-            this.gameObject.SetActive(false);
+            forcefield.SetActive(false);
         }
     }
+    
 }
