78e7d1712d18ea089a6b7156689d69e4c9d4746f
Chee Kit <cheekit.chong98@gmail.com>
2022-08-13 13:12:13 +0800

    fix(energy): freeze position x

--- Assets/Scripts/Maps/IntVector3DictVariable.cs
+++ Assets/Scripts/Maps/IntVector3DictVariable.cs
@@ -1,6 +1,7 @@
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = System.Random;
 
 [CreateAssetMenu(fileName = "IntVector3DictVariable", menuName = "ScriptableObjects/IntVector3DictVariable", order = 1)]
 public class IntVector3DictVariable : ScriptableObject
@@ -8,9 +9,14 @@ public class IntVector3DictVariable : ScriptableObject
 #if UNITY_EDITOR
     [Multiline] public string DeveloperDescription = "Dictionary with int key and Vector3 value";
 #endif
+    private readonly Random rand = new();
 
-    public Dictionary<int, Vector3> Value { get; private set; } = new Dictionary<int, Vector3>();
-    private System.Random rand = new System.Random();
+    public Dictionary<int, Vector3> Value { get; } = new();
+
+    public void Reset()
+    {
+        Value.Clear();
+    }
 
     public void AddItem(int index, Vector3 vector3)
     {
@@ -20,11 +26,6 @@ public class IntVector3DictVariable : ScriptableObject
     public void UpdateItem(int index, Vector3 newValue)
     {
         Value[index] = newValue;
-    }
-
-    public void Reset()
-    {
-        Value.Clear();
     }
 
     public void RemoveItem(int index)
@@ -39,8 +40,8 @@ public class IntVector3DictVariable : ScriptableObject
 
     public Vector3 GetRandomItem()
     {
-        List<Vector3> values = Enumerable.ToList(Value.Values);
-        int size = Value.Count;
+        var values = Value.Values.ToList();
+        var size = Value.Count;
         return values[rand.Next(size)];
     }
 }
