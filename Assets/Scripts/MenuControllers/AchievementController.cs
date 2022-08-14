a8ac997452f38cc1b1c9264d0a147d73b2963ac6
Yi Ern <61040996+bitterapple0@users.noreply.github.com>
2022-08-15 06:29:57 +0800

    Fix kill count bug

--- Assets/Scripts/MenuControllers/AchievementController.cs
+++ Assets/Scripts/MenuControllers/AchievementController.cs
@@ -5,7 +5,11 @@ public class AchievementController : MonoBehaviour
 {
     public int numPlayers = 4; 
     
-    public IntVariable[] PlayerStats;
+    // public IntVariable[] PlayerStats;
+    public IntVariable player1Stats;
+    public IntVariable player2Stats;
+    public IntVariable player3Stats;
+    public IntVariable player4Stats;
 
     public GameObject[] MostIcons;
 
@@ -18,26 +22,59 @@ public class AchievementController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Debug.Log("Achievement Controller start top, playerstats length: " + PlayerStats.Length);
+        // foreach (IntVariable i in PlayerStats) {
+        //     Debug.Log("IntVariable: " + i);
+        //     Debug.Log("Count: " + i.Value);
+        // }
+        Debug.Log("Player 1 stats intvariable: " + player1Stats);
+        Debug.Log("Player 1 stats value: " + player1Stats.Value);
+        Debug.Log("Player 2 stats intvariable: " + player2Stats);
+        Debug.Log("Player 2 stats value: " + player2Stats.Value);
+        Debug.Log("Player 3 stats intvariable: " + player3Stats);
+        Debug.Log("Player 3 stats value: " + player3Stats.Value);
+        Debug.Log("Player 4 stats intvariable: " + player4Stats);
+        Debug.Log("Player 4 stats value: " + player4Stats.Value);
         // testSetStats();
         getMaxMin();
+        Debug.Log("Max value: " + maxValue);
+        Debug.Log("Min value: " + minValue);
         updateIcons();
         // make sure all UI is updated first
         displayIcons();
+        // Debug.Log("Achievement Controller start bottom, playerstats length: " + PlayerStats.Length);
     }
     
     void getMaxMin(){
         // Get the max and min value from the scriptable objects
-        maxValue = PlayerStats[0].Value;
-        minValue = PlayerStats[0].Value;
-        for (int i = 1; i<numPlayers; i++)
-        {
-            if (PlayerStats[i].Value > maxValue)
-            {
-                maxValue = PlayerStats[i].Value;
-            }
-            if (PlayerStats[i].Value < minValue){
-                minValue = PlayerStats[i].Value;
-            }
+        maxValue = Mathf.Max(player1Stats.Value, player2Stats.Value);
+        maxValue = Mathf.Max(player3Stats.Value, maxValue);
+        maxValue = Mathf.Max(player4Stats.Value, maxValue);
+
+        minValue = Mathf.Min(player1Stats.Value, player2Stats.Value);
+        minValue = Mathf.Min(player3Stats.Value, minValue);
+        minValue = Mathf.Min(player4Stats.Value, minValue);
+        // maxValue = PlayerStats[0].Value;
+        // minValue = PlayerStats[0].Value;
+        // for (int i = 1; i<numPlayers; i++)
+        // {
+        //     if (PlayerStats[i].Value > maxValue)
+        //     {
+        //         maxValue = PlayerStats[i].Value;
+        //     }
+        //     if (PlayerStats[i].Value < minValue){
+        //         minValue = PlayerStats[i].Value;
+        //     }
+        // }
+    }
+
+    void updateIcon(IntVariable playerStats, GameObject mostIcon, GameObject leastIcon) {
+        if (playerStats.Value != maxValue) {
+            setIconBrightness(mostIcon);
+        }
+
+        if (playerStats.Value != minValue) {
+            setIconBrightness(leastIcon);
         }
     }
 
@@ -45,18 +82,22 @@ public class AchievementController : MonoBehaviour
     void updateIcons()
     {   
         // Loop through and set the brightness of the highest 
-        for (int i = 0; i<numPlayers; i++)
-        {
-            if (PlayerStats[i].Value != maxValue)
-            {
-                setIconBrightness(MostIcons[i]);
-            }
+        updateIcon(player1Stats,MostIcons[0], LeastIcons[0]);
+        updateIcon(player2Stats,MostIcons[1], LeastIcons[1]);
+        updateIcon(player3Stats,MostIcons[2], LeastIcons[2]);
+        updateIcon(player4Stats,MostIcons[3], LeastIcons[3]);
+        // for (int i = 0; i<numPlayers; i++)
+        // {
+        //     if (PlayerStats[i].Value != maxValue)
+        //     {
+        //         setIconBrightness(MostIcons[i]);
+        //     }
 
-            if (PlayerStats[i].Value != minValue)
-            {
-                setIconBrightness(LeastIcons[i]);
-            }
-        }
+        //     if (PlayerStats[i].Value != minValue)
+        //     {
+        //         setIconBrightness(LeastIcons[i]);
+        //     }
+        // }
     }
 
     // 'darken' the noobs so we know who actually won
@@ -66,12 +107,12 @@ public class AchievementController : MonoBehaviour
         icon.GetComponent<Image>().color = new Color(brightness,brightness,brightness);
     }
 
-    void testSetStats(){
-        PlayerStats[0].SetValue(0);
-        PlayerStats[1].SetValue(1);
-        PlayerStats[2].SetValue(3);
-        PlayerStats[3].SetValue(3);
-    }
+    // void testSetStats(){
+    //     PlayerStats[0].SetValue(0);
+    //     PlayerStats[1].SetValue(1);
+    //     PlayerStats[2].SetValue(3);
+    //     PlayerStats[3].SetValue(3);
+    // }
 
     void displayIcons(){
         foreach(Transform child in transform) 
--- Assets/Scripts/MenuControllers/WinnerController.cs
+++ Assets/Scripts/MenuControllers/WinnerController.cs
@@ -4,10 +4,23 @@ using UnityEngine.UI;
 public class WinnerController : MonoBehaviour
 {
     public int numPlayers = 4; 
-    
-    public IntVariable[] PlayerWinStats;
-    public IntVariable[] PlayerEnergyStats;
-    public IntVariable[] PlayerKillStats;
+    public IntVariable player1WinStats;
+    public IntVariable player2WinStats;
+    public IntVariable player3WinStats;
+    public IntVariable player4WinStats;
+
+    public IntVariable player1EnergyStats;
+    public IntVariable player2EnergyStats;
+    public IntVariable player3EnergyStats;
+    public IntVariable player4EnergyStats;
+
+    public IntVariable player1KillStats;
+    public IntVariable player2KillStats;
+    public IntVariable player3KillStats;
+    public IntVariable player4KillStats;
+    // public IntVariable[] PlayerWinStats;
+    // public IntVariable[] PlayerEnergyStats;
+    // public IntVariable[] PlayerKillStats;
 
 
     public GameObject[] PlayerIcons;
@@ -21,36 +34,53 @@ public class WinnerController : MonoBehaviour
     void Start()
     {
         // testSetStats();
+        // Debug.Log("WinnerController start (top), PlayerWinStats length: " + PlayerWinStats.Length);
         getMax();
         updateIcons();
         // make sure all UI is updated first
         displayIcons();
+        // Debug.Log("WinnerController start (bottom), PlayerWinStats length: " + PlayerWinStats.Length);
     }
     
     void getMax(){
         // Get the max and min value from the scriptable objects
-        maxValue = PlayerWinStats[0].Value;
-        for (int i = 1; i<numPlayers; i++)
-        {
-            if (PlayerWinStats[i].Value > maxValue)
-            {
-                maxValue = PlayerWinStats[i].Value;
-            }
+        // maxValue = PlayerWinStats[0].Value;
+        // for (int i = 1; i<numPlayers; i++)
+        // {
+        //     if (PlayerWinStats[i].Value > maxValue)
+        //     {
+        //         maxValue = PlayerWinStats[i].Value;
+        //     }
+        // }
+
+        maxValue = Mathf.Max(player1WinStats.Value, player2WinStats.Value);
+        maxValue = Mathf.Max(player3WinStats.Value, maxValue);
+        maxValue = Mathf.Max(player4WinStats.Value, maxValue);
+    }
+
+    void updateIcon(IntVariable playerStats, GameObject icon, GameObject crownIcon) {
+        if (playerStats.Value != maxValue) {
+            setIconBrightness(icon);
+            crownIcon.GetComponent<Image>().CrossFadeAlpha(0,0, true);
         }
     }
 
     // Update icons that should be 
     void updateIcons()
     {   
-        // Loop through and set the brightness of the highest 
-        for (int i = 0; i<numPlayers; i++)
-        {
-            if (PlayerWinStats[i].Value != maxValue)
-            {
-                setIconBrightness(PlayerIcons[i]);
-                CrownIcons[i].GetComponent<Image>().CrossFadeAlpha(0,0, true);
-            }
-        }
+        updateIcon(player1WinStats, PlayerIcons[0], CrownIcons[0]);
+        updateIcon(player1WinStats, PlayerIcons[1], CrownIcons[1]);
+        updateIcon(player1WinStats, PlayerIcons[2], CrownIcons[2]);
+        updateIcon(player1WinStats, PlayerIcons[3], CrownIcons[3]);
+        // // Loop through and set the brightness of the highest 
+        // for (int i = 0; i<numPlayers; i++)
+        // {
+        //     if (PlayerWinStats[i].Value != maxValue)
+        //     {
+        //         setIconBrightness(PlayerIcons[i]);
+        //         CrownIcons[i].GetComponent<Image>().CrossFadeAlpha(0,0, true);
+        //     }
+        // }
     }
 
     // 'darken' the noobs so we know who actually won
@@ -60,36 +90,57 @@ public class WinnerController : MonoBehaviour
         icon.GetComponent<Image>().color = new Color(brightness,brightness,brightness);
     }
 
-    void testSetStats(){
-        // PlayerWinStats[0].SetValue(3);
-        // PlayerWinStats[1].SetValue(1);
-        // PlayerWinStats[2].SetValue(2);
-        // PlayerWinStats[3].SetValue(0);
+    // void testSetStats(){
+    //     // PlayerWinStats[0].SetValue(3);
+    //     // PlayerWinStats[1].SetValue(1);
+    //     // PlayerWinStats[2].SetValue(2);
+    //     // PlayerWinStats[3].SetValue(0);
        
-        PlayerWinStats[0].SetValue(0);
-        PlayerWinStats[1].SetValue(3);
-        PlayerWinStats[2].SetValue(3);
-        PlayerWinStats[3].SetValue(3);
+    //     PlayerWinStats[0].SetValue(0);
+    //     PlayerWinStats[1].SetValue(3);
+    //     PlayerWinStats[2].SetValue(3);
+    //     PlayerWinStats[3].SetValue(3);
        
-        // PlayerWinStats[0].SetValue(0);
-        // PlayerWinStats[1].SetValue(1);
-        // PlayerWinStats[2].SetValue(3);
-        // PlayerWinStats[3].SetValue(2);
+    //     // PlayerWinStats[0].SetValue(0);
+    //     // PlayerWinStats[1].SetValue(1);
+    //     // PlayerWinStats[2].SetValue(3);
+    //     // PlayerWinStats[3].SetValue(2);
        
-        // PlayerWinStats[0].SetValue(0);
-        // PlayerWinStats[1].SetValue(1);
-        // PlayerWinStats[2].SetValue(2);
-        // PlayerWinStats[3].SetValue(3);
+    //     // PlayerWinStats[0].SetValue(0);
+    //     // PlayerWinStats[1].SetValue(1);
+    //     // PlayerWinStats[2].SetValue(2);
+    //     // PlayerWinStats[3].SetValue(3);
+    // }
+
+    void resetStats(IntVariable playerKillStat, IntVariable playerWinStat, IntVariable playerEnergyStat) {
+        playerKillStat.SetValue(0);
+        playerWinStat.SetValue(0);
+        playerEnergyStat.SetValue(0);
     }
 
     public void resetAllVariables()
     {
-        for (int i = 0; i < numPlayers; i++)
-        {
-            PlayerKillStats[i].SetValue(0);            
-            PlayerWinStats[i].SetValue(0);            
-            PlayerEnergyStats[i].SetValue(0);            
-        }
+        Debug.Log("resetting all variables");
+        resetStats(player1KillStats, player1WinStats, player1EnergyStats);
+        resetStats(player2KillStats, player2WinStats, player2EnergyStats);
+        resetStats(player3KillStats, player3WinStats, player3EnergyStats);
+        resetStats(player4KillStats, player4WinStats, player4EnergyStats);
+        // for (int i = 0; i < numPlayers; i++)
+        // {
+        //     PlayerKillStats[i].SetValue(0);            
+        //     PlayerWinStats[i].SetValue(0);            
+        //     PlayerEnergyStats[i].SetValue(0);            
+        // }
+        // Debug.Log("WinnerController: numPlayers: " + numPlayers);
+        // for (int i = 0; i < numPlayers; i++)
+        // {
+        //     Debug.Log(PlayerKillStats[i]);            
+        //     Debug.Log(PlayerKillStats[i].Value);            
+        //     Debug.Log(PlayerWinStats[i]);            
+        //     Debug.Log(PlayerWinStats[i].Value);            
+        //     Debug.Log(PlayerEnergyStats[i]);            
+        //     Debug.Log(PlayerEnergyStats[i].Value);            
+        // }
     }
 
     void displayIcons(){
